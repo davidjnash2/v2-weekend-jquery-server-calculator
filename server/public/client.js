d43c81ec8a01c2e1f49f9f7594e279dd835d6300
Dave Nash <davidjnash2@gmail.com>
2023-05-18 23:39:38 -0500

    updated clientjs event listener for operator button, which cleared out most if not all of my remaining console errors, and I can now see that frontend and backend are communicating, but i'm not actually capturing my input data, so will need to fix that next, and after that should be good on base requirements

--- server/public/client.js
+++ server/public/client.js
@@ -1,77 +1,79 @@
-console.log('clientjs ready! ');
+console.log('clientjs ready! '); // log to test
 
 $(document).ready(onReady);
 
 function onReady(){
-  console.log('sir, your jQuery has arrived');
-  $('#equals-button').on('click', postMaths);
-  $('#calculator-form').on('click', getOperator);
-  getMaths();
+  console.log('sir, your jQuery has arrived'); // log to test
+  $('#equals-button').on('click', postMaths); // event listener for equals button click
+  $('#operator-button').on('click', getOperator); // event listener for operator button click
+  getMaths(); // call getMaths function on load to populate DOM with any stored server info
 }
 
-let operator;
+let operator = ''; // declare unassigned operator variable 
 
-function getOperator(event){
-  event.preventDefault();
-  operator=$(this).text;
-  console.log('..and the operator is!', operator);
+function getOperator(event){ // start get operator function
+  event.preventDefault(); // prevent default browser response
+  operator=$(this).text; // select value of this clicked button
+  console.log('..and the operator is!', operator); // log to test
 } // end getOperator function
 
-function postMaths(event){
-  event.preventDefault();
-  console.log('in doMaths function');
-  let numOne = $('#first-number').val();
-  let numTwo = $('#second-number').val();
-  console.log(numOne, operator, numTwo);
-  $.ajax({
-    method: 'POST',
-    url: '/maths',
+function postMaths(event){ // start POST function
+  event.preventDefault(); // prevent default browser response
+  console.log('in doMaths function'); // log to test
+  let numOne = $('#first-number').val(); // capture value from first number input
+  let numTwo = $('#second-number').val(); // capture value from second number input
+  console.log('equation inputs are:', numOne, operator, numTwo); // log to test
+  $.ajax({ // ajax call
+    method: 'POST', // POST method
+    url: '/maths', // to url maths
     data: {
       numOne,
       operator,
       numTwo
-    },
+    }, // data object to be sent
+  }) // end ajax call
+  .then(function(response) { // await response from server
+    console.log('Response from server.', response); // log response to test
+    getMaths(); // call getMaths function to get updated equation list and render to DOM
   })
-  .then(function(response) {
-    console.log('Response from server.', response);
-    getMaths();
-  })
-  .catch(function(error) {
-    console.log('Error in POST', error)
-    alert('no can do maths rn');
-  });
+  .catch(function(error) { // error catch
+    console.log('Error in POST', error) // log error
+    alert('no can do maths rn'); // error alert
+  }); // end error catch
 } // end postMaths function
 
 function clearInputs(event){
-  console.log('in clearInputs function');
-  event.preventdefault();
-  $('#first-number').val('');
-  $('#second-number').val('');
+  console.log('in clearInputs function'); // log to test
+  event.preventdefault(); // prevent default browser response
+  $('#first-number').val(''); // empty first number input
+  $('#second-number').val(''); // empty second number input
 } // end clearInputs function
 
-function getMaths(){
-  console.log('in getMaths function');
-  $.ajax({
-    method: 'GET',
-    url: '/maths',
+function getMaths(){ // start getMaths function
+  console.log('in getMaths function'); // log to test
+  $.ajax({ // ajax call
+    method: 'GET', // GET path from server
+    url: '/maths', // at this url
+  }) // end ajax call
+  .then(function(response) { // call back response
+    console.log('maths from server are:', response);  // log to test
+    renderToDom(response); // update DOM
   })
-  .then(function(response) {
-    console.log('maths from server are:', response);
-    renderToDom(response);
-  })
-  .catch(function(error) {
-    console.log('Error in GET', error)
-    alert('no can do maths rn');
-  });
+  .catch(function(error) { // error catch
+    console.log('Error in GET', error)  // log error
+    alert('no can do maths rn'); // error alert
+  }); // end error catch
 } // end getMaths function
 
-function renderToDom(answers){
-  $('#answer-area').empty();
-  $('#past-math').empty();
+function renderToDom(answers){ // start render function
+  $('#answer-area').empty(); // empty answer field
+  $('#past-math').empty(); // empty equation history field
 
-  console.log('math history is:', answers)
+  console.log('math history is:', answers)  // log to test
+  // loop through answers, and append answer field with this result, and update equation history with full list
   for (let answer of answers){
-      $('#past-math').append(`
+    $('#answer-area').append(`${answer.result}`);
+    $('#past-math').append(`
       <li>${answer.numOne}${answer.operator}${answer.numTwo}=${answer.result}</li>
-  `)}
+  `)};
 } // end renderToDom function
