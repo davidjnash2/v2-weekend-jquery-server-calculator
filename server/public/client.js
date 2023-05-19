3b4e7270bbdca84445bd407ca35924e792ea82b5
Dave Nash <davidjnash2@gmail.com>
2023-05-18 23:23:13 -0500

    cleaned up formatting of clientjs, worked to consolidate and better format POST functoin, still some trouble with getting results to move from server to client, butgetting less errors, and will continue to work to resolve

--- server/public/client.js
+++ server/public/client.js
@@ -3,71 +3,75 @@ console.log('clientjs ready! ');
 $(document).ready(onReady);
 
 function onReady(){
-    console.log('sir, your jQuery has arrived');
-    $('#equals-button').on('click', postMaths);
-    $('#calculator-form').on('click', getOperator);
-    getMaths();
+  console.log('sir, your jQuery has arrived');
+  $('#equals-button').on('click', postMaths);
+  $('#calculator-form').on('click', getOperator);
+  getMaths();
 }
 
 let operator;
 
 function getOperator(event){
-    event.preventDefault();
-    operator=$(this).text;
-    console.log('..and the operator is!', operator);
+  event.preventDefault();
+  operator=$(this).text;
+  console.log('..and the operator is!', operator);
 } // end getOperator function
 
 function postMaths(event){
-    event.preventDefault();
-    console.log('in doMaths function');
-    let numOne = $('#first-number').val();
-    let numTwo = $('#second-number').val();
-    console.log(numOne, operator, numTwo);
-    $.ajax({
-        method: 'POST',
-        url: '/maths',
-        data: {
-            numOne,
-            operator,
-            numTwo
-        },
-        }).then(function(response) {
-          console.log('Response from server.', response);
-          getMaths();
-        }).catch(function(error) {
-          console.log('Error in POST', error)
-          alert('no can do maths rn');
-        });
+  event.preventDefault();
+  console.log('in doMaths function');
+  let numOne = $('#first-number').val();
+  let numTwo = $('#second-number').val();
+  console.log(numOne, operator, numTwo);
+  $.ajax({
+    method: 'POST',
+    url: '/maths',
+    data: {
+      numOne,
+      operator,
+      numTwo
+    },
+  })
+  .then(function(response) {
+    console.log('Response from server.', response);
+    getMaths();
+  })
+  .catch(function(error) {
+    console.log('Error in POST', error)
+    alert('no can do maths rn');
+  });
 } // end postMaths function
 
 function clearInputs(event){
-    console.log('in clearInputs function');
-    event.preventdefault();
-    $('#first-number').val('');
-    $('#second-number').val('');
+  console.log('in clearInputs function');
+  event.preventdefault();
+  $('#first-number').val('');
+  $('#second-number').val('');
 } // end clearInputs function
 
 function getMaths(){
-    console.log('in getMaths function');
-    $.ajax({
-        method: 'GET',
-        url: '/maths',
-        })
-        .then(function(response) {
-          console.log('maths from server are:', response);
-          renderToDom();
-        })
-        .catch(function(error) {
-          console.log('Error in GET', error)
-          alert('no can do maths rn');
-        });
+  console.log('in getMaths function');
+  $.ajax({
+    method: 'GET',
+    url: '/maths',
+  })
+  .then(function(response) {
+    console.log('maths from server are:', response);
+    renderToDom(response);
+  })
+  .catch(function(error) {
+    console.log('Error in GET', error)
+    alert('no can do maths rn');
+  });
 } // end getMaths function
 
-function renderToDom(result){
-    $('#past-math').empty();
-    console.log('math history is:', result)
-    for (let answer of result){
-        $('#past-math').append(`
-        <li>${answer.numOne}${answer.operator}${answer.numTwo}=${answer.answer}</li>
-    `)}
+function renderToDom(answers){
+  $('#answer-area').empty();
+  $('#past-math').empty();
+
+  console.log('math history is:', answers)
+  for (let answer of answers){
+      $('#past-math').append(`
+      <li>${answer.numOne}${answer.operator}${answer.numTwo}=${answer.result}</li>
+  `)}
 } // end renderToDom function
--- server/server.js
+++ server/server.js
@@ -12,42 +12,44 @@ app.listen(PORT, () => {
   
 let answers = [];
 
-function doMaths(inputs) {
-    if (inputs.operator === '+'){
-      inputs.result = Number(inputs.num1) + Number(inputs.num2);
-    } else if (inputs.operator === '-'){
-      inputs.result = Number(inputs.num1) - Number(inputs.num2);          
-    } else if (inputs.operator === '*'){
-      inputs.result = Number(inputs.num1) * Number(inputs.num2); 
-    } else if (inputs.operator === '/') {
-      inputs.result = Number(inputs.num1) / Number(inputs.num2);
-    }
-
-answers.push(inputs);
-}
 app.get('/maths', function(req,res) { // start GET
-    console.log('GET /maths request made', answers); // log to test
+    console.log('GET /maths', answers); // log to test
     res.send(answers); // send back answers array as result
-  }) // end GET
+}) // end GET
   
   
   
-  app.post('/maths', function(req,res) { // start POST
-    let numOne = req.body.numOne; // declare variable for incoming parsed data
+app.post('/maths', function(req,res) { // start POST
+    console.log('POST /maths at work'); // log to test
+    console.log('req.body=', req.body); // log to test
+
+    let numOne = Number(req.body.numOne); // declare variable for incoming parsed data
     let operator = req.body.operator; // declare variable for incoming parsed data
-    let numTwo = req.body.numTwo; // declare variable for incoming parsed data
-  
-    console.log('POST /maths request made'); // log to test
-    console.log('req.body is :', req.body); // log to test
-  
-    let inputs = { // declare joke object using above values for inputs
+    let numTwo = Number(req.body.numTwo); // declare variable for incoming parsed data
+    
+    let result = '';
+
+
+    if (operator === '+'){
+        result = numOne + numTwo;
+      } else if (operator === '-'){
+        result = numOne - numTwo;          
+      } else if (operator === '*'){
+        result = numOne * numTwo; 
+      } else if (operator === '/'){
+        result = numOne / numTwo;
+      }
+
+
+    let problem = { // declare problem object using above values for inputs
       numOne,
       operator,
-      numTwo
+      numTwo,
+      result
       }
-    answers.push(inputs); // push new jokes to array
-    res.sendStatus(201); // send ok to client
-  }) // end POST
+    answers.push(problem); // push each new problem to answers array
+    res.sendStatus(201); // serve all good to client
+}) // end POST maths function
   
 
 
